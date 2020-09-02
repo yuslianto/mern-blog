147c367252e0ce984c025086de1d38f72a2c8065
yuslianto <yuslianto56@gmail.com>
2020-09-02 07:04:02 +0700

    konsep SCSS Global dan slicing hallaman login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import { LoginBg } from '../../assets'
+import {Input, Button, Gap, Link} from '../../components'
+
 const Login = () => {
     return (
-        <div>
-            <p>login pages</p>
+        <div className="main-page">
+            <div className="left">
+                <img src={LoginBg} className="bg-image" alt="image Bg"/>
+            </div>
+            <div className="right">
+                <p className="title">Form Login</p>
+                <Input label="Email" placeholder="Email"/>
+                <Gap height={18} />
+                <Input label="Password" placeholder="Password"/>
+                <Gap height={50}/>
+                <Button title="Login"/>
+                <Gap height={100} />
+                <Link title="Belum punya alun, silahkan daftar"/>
+            </div>
         </div>
     )
 }
