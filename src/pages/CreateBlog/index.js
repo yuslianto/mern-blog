f78fae0d321d946d16b42fda3f4cd5630d870063
yuslianto <yuslianto56@gmail.com>
2020-09-05 00:29:16 +0700

    membuat styling cfeate blog post

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Input, Button, Upload, TextArea, Gap} from "../../components"
+import "./createBlog.scss"
 
 const CreateBlog = () => {
     return (
-        <div>
-            <p>Content create your blog</p>
+        <div className="blog-post">
+            <p className="title">Create New Blog Postg</p>
+            <Input labul="Post Title" />
+            <Upload />
+            <TextArea/>
+            <Gap height={20}/>
+            <div className="button-action">
+                <Button title="Save" />
+            </div>
         </div>
     )
 }
