f40e08ea50480e50706f11c3e88e1473413d4b45
yuslianto <yuslianto56@gmail.com>
2020-09-09 16:22:26 +0700

    membuat styling halaman detail blog post

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {RegisterBg} from '../../assets'
+import './detailBlog.scss'
 
 const DetailBlog = () => {
     return (
-        <div>
-            <p>Content detail your blog</p>
+        <div className="detail-blog-wrapper">
+            <img className="img-title" src={RegisterBg} alt="img" />
+            <p className="blog-title">Title blog</p>
+            <p className="blog-author">Author - Date Post</p>
+            <p className="blog-body">lorem ipsum is simply dummy text of Magna esse nulla 
+                labore et consequat Lorem reprehenderit et ad.
+                Et duis non irure sit reprehenderit incididunt amet.</p>
         </div>
     )
 }
